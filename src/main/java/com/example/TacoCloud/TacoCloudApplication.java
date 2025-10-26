677c9bc66ed810a0635557962d4c5f886f702e1a
shakil <me.mahadi.hasan.shakil@gmail.com>
2025-10-26 11:37:06 +0600

    Chapter-1: Initialize project, add home controller and tested @WebMvcTes

---
+++ src/main/java/com/example/TacoCloud/TacoCloudApplication.java
@@ -0,0 +1,19 @@
+package com.example.TacoCloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TacoCloudApplication.class, args);
+
+		log.info("Taco Cloud Application Started.");
+
+	}
+
+}
