f3236500141344118720e2e2818936148bec3e4d
mahadi.hasan <mahadi.hasan@bkash.com>
2025-10-27 17:18:22 +0600

    finishing up chapter-2

--- src/main/java/com/example/TacoCloud/controller/OrderController.java
+++ src/main/java/com/example/TacoCloud/controller/OrderController.java
@@ -1,12 +1,13 @@
 package com.example.TacoCloud.controller;
 
+import com.example.TacoCloud.pojo.Taco;
 import com.example.TacoCloud.pojo.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
@@ -16,12 +17,20 @@ import org.springframework.web.bind.support.SessionStatus;
 public class OrderController {
 
     @GetMapping("/current")
-    public String orderFrom() {
+    public String orderFrom(Model model) {
+        if (!model.containsAttribute("tacoOrder")) {
+            model.addAttribute("tacoOrder", new TacoOrder());
+        }
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order,
+                               Errors errors,
+                               SessionStatus sessionStatus) {
+        if(errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
